7a597a60d5f46fec78ded7aee19d77143d9a506c
JCamiloCardonaZ <juancamilo.cardona1@utp.edu.co>
2025-02-25 23:01:12 -0500

    creacion Relaciones y Reglas

--- coronel.pl
+++ coronel.pl
@@ -7,3 +7,18 @@ nacion_hostil(corea_del_Sur).
 vender_armas(X, _) :- estadounidense(X), nacion_hostil(Y),
 	vendio_misiles(X,Y).
 
+% Los misiles de corea del sur fueron vendidos por el coronel
+tiene(corea_del_Sur, misil1).
+tiene(corea_del_Sur, misil2).
+tiene(corea_del_Sur, misil3).
+vendio(coronel_west, misil1).
+vendio(coronel_west, misil2).
+vendio(coronel_west, misil3).
+
+%regla para comprobar la venta
+vendio_misiles(X,Y):- tiene(Y,M), vendio(X,M).
+
+%regla para identificar criminal
+criminal(X) :- vender_armas(X, _).
+
+

52a5e5c2cfe9ab538af4a86083ce317f18a21655
Juan Camilo Gallego <camilo.gallego3@utp.edu.co>
2025-02-25 22:56:18 -0500

    hechos coronel y primer regla

--- coronel.pl
+++ coronel.pl
@@ -1,1 +1,9 @@
-%
+estadounidense(coronel_west).
+nacion_hostil(corea_del_Sur).
+
+
+%regla para ventas de armas
+
+vender_armas(X, _) :- estadounidense(X), nacion_hostil(Y),
+	vendio_misiles(X,Y).
+

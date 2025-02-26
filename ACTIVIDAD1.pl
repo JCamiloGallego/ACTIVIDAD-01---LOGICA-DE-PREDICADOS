da91e3fddf7c7158d63f81b5cb818e4a3896c26a
Juan Camilo Gallego <camilo.gallego3@utp.edu.co>
2025-02-25 19:53:00 -0500

    Hecho- relacion padres(X,Y)

---
+++ ACTIVIDAD1.pl
@@ -0,0 +1,10 @@
+%hechos
+
+padre(abraham, homero).
+padre(abraham, herbert).
+padre(clancy, marge).
+padre(clancy, patty).
+padre(clancy, selma).
+padre(homero, bart).
+padre(homero, lisa).
+padre(homero, maggie).
